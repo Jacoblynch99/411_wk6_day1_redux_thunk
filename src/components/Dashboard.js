e0b5d123d93cf27e913cd8285640c47b9514b4aa
Jacoblynch99 <jacoblynch99@gmail.com>
2020-10-13 10:24:06 -0500

    turnin

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,13 +5,13 @@ import {
     TableBody,
     TableCell,
     TableHead,
-    TableRow
+    TableRow,
 } from '@material-ui/core'
 import DeleteIcon from '@material-ui/icons/Delete'
 import Chart from '../containers/Chart'
 import Total from '../containers/Total'
 import AddCar from '../containers/AddCar'
- 
+
 const Dashboard = (props) => {
     return (
         <Container maxWidth="lg" className="car-container">
@@ -33,23 +33,24 @@ const Dashboard = (props) => {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                {props.cars.map((car, idx) => (
-                    <TableRow key={car.id}>
-                        <TableCell component="th" scope="row">
-                            {car.id}
-                        </TableCell>
-                        <TableCell>{car["name"]}</TableCell>
-                        <TableCell>{car["mpg"]}</TableCell>
-                        <TableCell>{car["cylinders"]}</TableCell>
-                        <TableCell>{car["horsepower"]}</TableCell>
-                        <TableCell>
-                            <DeleteIcon
-                                // add onClick method here
-                                onClick={() => props.removeCar(idx)}
-                                className="icon text-red" />
-                        </TableCell>
-                    </TableRow>
-                ))}
+                    {props.cars.map((car, idx) => (
+                        <TableRow key={car.id}>
+                            <TableCell component="th" scope="row">
+                                {car.id}
+                            </TableCell>
+                            <TableCell>{car['name']}</TableCell>
+                            <TableCell>{car['mpg']}</TableCell>
+                            <TableCell>{car['cylinders']}</TableCell>
+                            <TableCell>{car['horsepower']}</TableCell>
+                            <TableCell>
+                                <DeleteIcon
+                                    // add onClick method here
+                                    onClick={() => props.removeCar(idx)}
+                                    className="icon text-red"
+                                />
+                            </TableCell>
+                        </TableRow>
+                    ))}
                 </TableBody>
             </Table>
         </Container>
--- src/components/Import.js
+++ src/components/Import.js
@@ -1,10 +1,89 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Paper,
+    Button,
+    Container,
+    Menu,
+    MenuItem,
+} from '@material-ui/core'
+import { MoreVert } from '@material-ui/icons'
+import DeleteIcon from '@material-ui/icons/Delete'
 
 const Import = (props) => {
     // fill out this component
 
+    const [anchorEl, setAnchorEl] = useState(null)
+    const [idNum, setIdNum] = useState(null)
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget)
+        setIdNum(event.currentTarget.id)
+    }
+
+    const handleClose = () => {
+        setAnchorEl(null)
+    }
+
     return (
-        <p>Import Component</p>
+        <Container>
+            <Button
+                onClick={props.fetchMakes}
+                variant="contained"
+                color="primary"
+                style={{ marginTop: 25 }}
+            >
+                IMPORT
+            </Button>
+            <h2>COUNT: {props.makes.length}</h2>
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Id</TableCell>
+                            <TableCell align="left">Make</TableCell>
+                            <TableCell align="right">Actions</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {props.makes.map((item, id) => {
+                            return (
+                                <TableRow key={item.MakeName}>
+                                    <TableCell>{item.MakeId}</TableCell>
+                                    <TableCell align="left">
+                                        {item.MakeName}
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        <Button id={id} onClick={handleClick}>
+                                            <MoreVert />
+                                        </Button>
+                                    </TableCell>
+                                </TableRow>
+                            )
+                        })}
+                    </TableBody>
+                </Table>
+                <Menu
+                    id="simple-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                >
+                    <MenuItem onClick={handleClose}>
+                        <Button onClick={() => props.removeEntry(idNum)}>
+                            <DeleteIcon />
+                            Delete
+                        </Button>
+                    </MenuItem>
+                </Menu>
+            </TableContainer>
+        </Container>
     )
 }
 
